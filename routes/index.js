9463fb543e7bbaabbc1d2725260edb17d32d537d
chris-malloy <chrismalloymusic@gmail.com>
2017-10-31 09:13:29 -0400

    practice with routes/views

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,40 @@ router.get('/', function(req, res, next) {
             imageBaseUrl: imageBaseUrl
         });
     });
-    // res.render('index', { title: 'Movie App' });
+});
+
+router.post('/search', (req, res) => {
+    request.get(nowPlayingUrl, (error, response, movieData) => {
+        var userSearchTerm = req.body.movieSearch;
+        var userSearchActor = req.body.actorSearch;
+        // req.query.key fetches query value of key from request...
+        //look to index.ejs to action value of form tag
+        var queryString = req.query.key;
+        var parsedData = JSON.parse(movieData);
+        res.render('index', {
+            parsedData: parsedData.results,
+            imageBaseUrl: imageBaseUrl
+        });
+    });
+})
+
+//if you have /: that part of th path is Wild!
+//in this case, /movie/:movieId will trigger /movie/ANYTHING
+// to access the ANYTHING, you go tot req.params.ANYTHING
+// this makes your urls cleaner
+
+router.get('/movie/:movieId', (req, res) => {
+    // res.json(req.params);
+    var movieId = req.params.movieId;
+    var thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_Key=${config.apiKey}`
+    request.get(thisMovieUrl, (error, response, movieData) => {
+        var parsedData = JSON.parse(movieDta);
+        // res.json(parsedData);
+        res.render('single-movie', {
+            movieData: parsedData,
+            imageBaseUrl: imageBaseUrl
+        })
+    })
 });
 
 module.exports = router;
